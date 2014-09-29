256ca6b3866bfc0d5547996105e757d0f8b348d5
Nathan LeClaire <nathan.leclaire@gmail.com>
2014-09-28 20:44:59 -0700

    Add pretty colors

--- chip_away_plugins.sh
+++ chip_away_plugins.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
+# Reset
+Color_Off='\e[0m'       # Text Reset
+
+# High Intensity
+IBlack='\e[0;90m'       # Black
+IRed='\e[0;91m'         # Red
+IGreen='\e[0;92m'       # Green
+IYellow='\e[0;93m'      # Yellow
+IBlue='\e[0;94m'        # Blue
+IPurple='\e[0;95m'      # Purple
+ICyan='\e[0;96m'        # Cyan
+IWhite='\e[0;97m'       # White
+
 echo "stashing working directory changes..."
 STASH_MSG=$(git stash)
 for plugin in plugins/*; do
-    echo "=> Trying out removing plugin ${plugin}..."
+    echo "${ICyan}=> Trying out removing plugin ${plugin}..."
 
     # invalidate public diff so that failed generate will get caught
     echo "invalidate" >>public/index.html
 
     git rm ${plugin}
-    ./build.sh 2>&1
+    ./build.sh &>/dev/null
 
     # remove XML timestamp stuff (not what we're interested in)
     find public -name *.xml | xargs -n 3 git checkout --
 
     DIFF=$(git diff)
     if [[ "$DIFF" != "" ]]; then
-        echo "==> There was a difference after removing the plugin ${plugin}!"
+        echo "${IRed}==> There was a difference after removing the plugin ${plugin}!"
         git reset HEAD ${plugin}
         git checkout -- ${plugin} public/
-        exit 1
+    else
+        echo "${IGreen}Great success removing plugin!"
     fi
 done
 echo "restoring working directory changes..."
