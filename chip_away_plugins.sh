15719d6a02e94342e909dab23b7de4351e37e049
Nathan LeClaire <nathan.leclaire@gmail.com>
2014-09-28 21:19:54 -0700

    Get incredibly fancy with subcommands

--- chip_away_plugins.sh
+++ chip_away_plugins.sh
@@ -12,28 +12,41 @@ LIGHTGRAY='\033[00;37m'
 
 echo "stashing working directory changes..."
 STASH_MSG=$(git stash)
-for plugin in plugins/*; do
-    echo -e "${CYAN}=> Trying out removing plugin ${plugin}...${RESTORE}"
 
-    # invalidate public diff so that failed generate will get caught
-    echo "invalidate" >>public/index.html
+chip_away_at_plugins () {
+    for plugin in plugins/*; do
+        echo -e "|=> ${PURPLE}Trying out removing plugin ${plugin}...${RESTORE}"
 
-    git rm ${plugin}
-    ./build.sh &>/dev/null
+        # invalidate public diff so that failed generate will get caught
+        echo "invalidate" >>public/index.html
 
-    # remove XML timestamp stuff (not what we're interested in)
-    find public -name *.xml | xargs -n 3 git checkout --
+        printf "====> " && git rm ${plugin}
+        ./build.sh &>/dev/null
 
-    DIFF=$(git diff)
-    if [[ "$DIFF" != "" ]]; then
-        echo -e "====> ${RED} There was a difference after removing the plugin ${plugin}!${RESTORE}"
-        git reset HEAD ${plugin} >/dev/null
-        git checkout -- ${plugin} public/ >/dev/null
-    else
-        echo -e "====> ${GREEN}Great success removing plugin!${RESTORE}"
+        # remove XML timestamp stuff (not what we're interested in)
+        find public -name *.xml | xargs -n 3 git checkout --
+
+        DIFF=$(git diff)
+        if [[ "$DIFF" != "" ]]; then
+            echo -e "====> ${RED} There was a difference after removing the plugin ${plugin}!${RESTORE}"
+            git reset HEAD ${plugin} >/dev/null
+            git checkout -- ${plugin} public/ >/dev/null
+        else
+            echo -e "====> ${GREEN}Great success removing plugin ${plugin}!${RESTORE}"
+        fi
+    done
+    echo "restoring working directory changes..."
+    if [[ "$STASH_MSG" != "No local changes to save" ]]; then
+        git stash pop >/dev/null
     fi
-done
-echo "restoring working directory changes..."
-if [[ "$STASH_MSG" != "No local changes to save" ]]; then
-    git stash pop >/dev/null
-fi
+}
+
+case "$1" in 
+    apply)
+        chip_away_at_plugins
+    rollback)
+        git reset HEAD plugins/
+        git checkout -- plugins
+    *)
+        echo "Usage: $0 {apply|rollback}"
+esac
