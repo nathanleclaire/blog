d08dedeb5ff6d97ef0628c4f4b2e9de66e9b063c
Nathan LeClaire <nathan.leclaire@gmail.com>
2014-09-28 20:41:23 -0700

    Another addition to the chip away plugin script

--- chip_away_plugins.sh
+++ chip_away_plugins.sh
@@ -4,14 +4,22 @@ echo "stashing working directory changes..."
 STASH_MSG=$(git stash)
 for plugin in plugins/*; do
     echo "=> Trying out removing plugin ${plugin}..."
+
+    # invalidate public diff so that failed generate will get caught
+    echo "invalidate" >>public/index.html
+
     git rm ${plugin}
-    ./build.sh 2&>1  >/dev/null
-    echo "==> Done building, checking for differences."
+    ./build.sh 2>&1
+
+    # remove XML timestamp stuff (not what we're interested in)
+    find public -name *.xml | xargs -n 3 git checkout --
+
     DIFF=$(git diff)
     if [[ "$DIFF" != "" ]]; then
         echo "==> There was a difference after removing the plugin ${plugin}!"
         git reset HEAD ${plugin}
         git checkout -- ${plugin} public/
+        exit 1
     fi
 done
 echo "restoring working directory changes..."
