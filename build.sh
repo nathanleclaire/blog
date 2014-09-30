87dd2542ac440e8e907d2f8bbbba8513ef345880
Nathan LeClaire <nathan.leclaire@gmail.com>
2014-09-30 00:06:25 -0700

    Rm public before build

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-docker build -t nathanleclaire/octoblog .
 if [[ -d public ]]; then
     echo "removing public/ directory..."
     rm -rf public
 fi
 
-# extreme hack to get exit code
+docker build -t nathanleclaire/octoblog .
 docker run nathanleclaire/octoblog sh -c "rake install['pageburner'] && rake generate"
 LAST_CONTAINER=$(docker ps -lq)
 docker cp ${LAST_CONTAINER}:/blog/public .
